b30acc41d1fd40535769bdf2d37c5db331f6d802
aylinsglam <73175714+aylinsglam@users.noreply.github.com>
2020-11-19 22:36:52 +0300

    Update sor_4_1.py

--- sor_4_1.py
+++ sor_4_1.py
@@ -1,5 +1,8 @@
-def fibonacci():
+def fibonacci(n):
     a,b=0,1
-    for i in range(0,30):
+    for i in range(0,n):
+        a,b=b,a+b   
         print(a,end=" ")
-        a,b=b,a+b
+fibonacci(30)
+
+        
